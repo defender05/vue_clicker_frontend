5b0b5c52db68d7af038b4aaffc570f3dce900c70
defender05 <alil.a@mail.ru>
2024-07-30 18:03:34 +0300

    update 2

--- src/App.vue
+++ src/App.vue
@@ -51,12 +51,37 @@ const showTabbar = computed(() => {
 
     <div id="footer">
       
-      <van-tabbar v-show="showTabbar" v-model="active" active-color="#FF7618">
-        <van-tabbar-item icon="home-o" to="/home">Главная</van-tabbar-item>
-        <van-tabbar-item icon="search" to="/mine">Кликер</van-tabbar-item>
-        <van-tabbar-item icon="search" to="/shop">Магазин</van-tabbar-item>
-        <van-tabbar-item icon="search" to="/rating">Рейтинг</van-tabbar-item>
+      <van-tabbar v-show="showTabbar" v-model="active" class="menu_bar">
+    
+        <van-tabbar-item to="/home" class="menu_bar_item">
+          <div class="flex_col">
+            <div><img src="./assets/home.svg" alt="" class="menu_item_img"></div>
+            <div class="menu_item_text">Главная</div>
+          </div>
+        </van-tabbar-item>
+
+        <van-tabbar-item to="/mine" class="menu_bar_item">
+          <div class="flex_col">
+            <div><img src="./assets/clicker.svg" alt="" class="menu_item_img"></div>
+            <div class="menu_item_text">Кликер</div>
+          </div>
+        </van-tabbar-item>
+
+        <van-tabbar-item to="/shop" class="menu_bar_item">
+          <div class="flex_col">
+            <div><img src="./assets/shop.svg" alt="" class="menu_item_img"></div>
+            <div class="menu_item_text">Магазин</div>
+          </div>
+        </van-tabbar-item>
+
+        <van-tabbar-item to="/rating" class="menu_bar_item">
+          <div class="flex_col">
+            <div><img src="./assets/rating.svg" alt="" class="menu_item_img"></div>
+            <div class="menu_item_text">Рейтинг</div>
+          </div>
+        </van-tabbar-item>
       </van-tabbar>
+
     </div>
   </div>
 </template>
@@ -72,8 +97,32 @@ const showTabbar = computed(() => {
   z-index: 0;
   height: 100vh;
 }
-:root {
-  --van-tabbar-height: 100;
+/* :root:root {
+  --van-tabbar-height: 100px;
+} */
+.menu_bar {
+  border: 0;
+  background-color: #131212;
+  height: 60px;
+  
+}
+.menu_item_img {
+  height: 25px;
+  width: 25px;
+  color: #ffffff;
+}
+.menu_item_text {
+  color: #868686;
+  margin-top: 3px;
+  font-size: 1em;
+  font-weight: 400;
+}
+.menu_bar_item {
+  padding-bottom: 5px;
+}
+.menu_bar_item:focus {
+  background-color: #000000;
+  color:#FF7618;
 }
 
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,6 +18,7 @@ onMounted(async () => {
     user_avatar.value = user.country.image_url;
     user_balance.value = user.game_balance;
     user_capacity.value = user.total_capacity;
+    
     // console.log("UserData", JSON.stringify(user));
     
 });
@@ -28,7 +29,15 @@ onMounted(async () => {
     <div class="home_content">
 
         <div class="user_info flex_row">
-            <img :src="user_avatar" class="avatar_image">
+            <!-- <van-image
+                :src="user_avatar"
+                class="avatar_image"
+            /> -->
+            <div class="image_container">
+                <img v-if="user_avatar" :src="user_avatar" class="avatar_image">
+                <van-skeleton v-else title avatar :row="0" />
+            </div>
+   
             <span class="user_name">{{ user_name }}</span>
             <img src="../assets/user_edit.svg" class="user_edit_image">
         </div>
@@ -40,18 +49,22 @@ onMounted(async () => {
                     <img src="../assets/coin.svg" class="balance_coin_image">
                 </div>
 
-                <div class="flex_row">
-                    <span style="color: #FFFFFF; font-weight: 100;">Баланс</span>
-                </div>
+                <!-- <div class="flex_row">
+                    <span class="balance_text">Баланс</span>
+                </div> -->
 
                 <div class="stat_row flex_row">
                     <div class="stat_box flex-col">
-                        <div class="flex_row"><span class="stat_box_number">{{ user_capacity }}</span><span class="stat_box_unit">т/с</span></div>
-                        <span class="stat_box_title">Производительность</span>
+                        <div class="flex_row">
+                            <span v-if="user_capacity < 10000" class="stat_box_number">{{ user_capacity }}</span>
+                            <span v-else class="stat_box_number">{{ user_capacity / 1000 }}k</span>
+                            <span class="stat_box_unit">т/ч</span>
+                        </div>
+                        <div class="stat_box_title">Производительность</div>
                     </div>
                     <div class="stat_box flex-col">
                         <div><span class="stat_box_number">{{ 12000 }}+</span></div>
-                        <div><span class="stat_box_title">Место в рейтинге</span></div>
+                        <div class="stat_box_title">Место в рейтинге</div>
                     </div>
                 </div>
             </div>
@@ -66,6 +79,7 @@ onMounted(async () => {
     display: flex;
     flex-direction: column;
     padding: 0% 5%;
+    gap: 20px;
 }
 
 /* СТРОКА - Имя и ватарка юзера */
@@ -75,32 +89,52 @@ onMounted(async () => {
     width: 100%;
 }
 .user_name {
+    font-size: 1.5em;
+    font-weight: 400;
     color:rgb(255, 255, 255);
     margin-right: auto;
 }
+.image_container {
+    width: 50px; /* Устанавливаем нужную ширину контейнера */
+    height: 50px; /* Устанавливаем нужную высоту контейнера */
+    overflow: visible; /* Скрываем часть изображения, выходящую за пределы контейнера */
+    margin: 0px 15px;
+}
 .avatar_image {
     width: 100px;
-    height: 100px;
+    height: auto; 
+    object-fit: contain; /* Сохраняем пропорции изображения */
+    object-position: center; /* Центрируем изображение в контейнере */
+    transform-origin: center;
+    transform: translate(-25px, -22px);
 }
 .user_edit_image {
     width: 35px;
     height: 35px;
+    margin-right: 30px;
 }
 
 /* СТРОКА - Баланс, место в рейтинге и производительность */
 .main_info {
     background-color: #0C0C0C;
     border: 1px solid #1F1F1F;
-    border-radius: 10px;
-    padding: 10px;
+    border-radius: 20px;
+    padding: 20px;
 }
 .balance_row {
     align-items: center;
+    /* justify-content: space-between; */
 }
 .user_game_balance {
     font-size: 2em;
+    font-weight: 500;
     color: #FF7618;
     margin-right: 10px;
+}
+.balance_text {
+    color: #5c5b5b;
+    font-size: 1em;
+    font-weight: 100;
 }
 .balance_coin_image {
     width: 25px;
@@ -109,25 +143,28 @@ onMounted(async () => {
 .stat_row {
     margin-top: 10px;
     justify-content: space-between;
+    gap: 15px;
 }
 .stat_box {
     width: 100%;
     background-color: #0C0C0C;
-    border: 1px solid #1F1F1F;
-    border-radius: 10px;
-    padding: 5px 10px;
+    border: 1.5px solid #1f1f1f7a;
+    border-radius: 8px;
+    padding: 10px;
+    text-align: justify;
 }
 .stat_box_title {
-    font-size: 0.7em;
+    font-size: 90%;
     color: #6B6B6B;
 }
 .stat_box_number {
-    font-size: 1.2em;
+    font-size: 1.5em;
+    font-weight: 500;
     color: #ffffff;
     margin-right: 5px;
 }
 .stat_box_unit {
-    font-size: 1.2em;
+    font-size: 1.5em;
     color: #FF7618;
 }
 </style>
